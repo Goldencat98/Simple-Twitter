a5caa484b41365ffac9ee67fe4aee3bf0cd0f40a
goldencat <codingwithgoldencat@gmail.com>
2024-08-10 20:29:45 +0530

    project start

--- src/main/java/com/example/twitterapp/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.twitterapp.repository;
-
-import com.example.twitterapp.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
